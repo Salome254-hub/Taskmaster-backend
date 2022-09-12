8839942b213d9586599c6b6b479c75d13a390897
Salome <sallynanjala254@gmail.com>
2022-09-12 10:51:32 +0300

    Added a post route to my controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,29 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get '/tasks' do
+  get '/' do
     tasks=Task.all
     tasks.to_json
   end
+
+  post '/' do
+    message=Task.create(body:params[:body],username:params[:username])
+    message.to_json
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
