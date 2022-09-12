2dc024887d02ff3d58e3f9b89825e51b2f6b9843
Salome <sallynanjala254@gmail.com>
2022-09-12 10:14:48 +0300

    Modified my task migration

--- db/migrate/20220912071039_create_tasks.rb
+++ db/migrate/20220912071039_create_tasks.rb
@@ -1,4 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
-  def change
+
+    def change
+      create_table :tasks do |t|
+        t.string :body
+        t.string :username
+        t.timestamps
+      end
+    end
   end
-end
